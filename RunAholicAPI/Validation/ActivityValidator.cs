40589d1a86cc015930dbc5275a6ea0083454cf5b
JarneDemoen <jarne.demoen@student.howest.be>
2022-04-26 12:13:41 +0200

    Beginnen met graphql en test

--- RunAholicAPI/Validation/ActivityValidator.cs
+++ RunAholicAPI/Validation/ActivityValidator.cs
@@ -4,16 +4,20 @@ public class ActivityValidator : AbstractValidator<Activity>
 {
     public ActivityValidator()
     {
-        RuleFor(a => a.Name).NotEmpty().WithMessage("Name is empty");
         RuleFor(a => a.Name).NotNull().WithMessage("Name is not included in the body");
+        RuleFor(a => a.Name).NotEmpty().WithMessage("Name is empty");
+
         RuleFor(a => a.StartDateLocal).NotNull().WithMessage("StartDateLocal is not included in the body");
         RuleFor(a => a.StartDateLocal).NotEmpty().WithMessage("StartDateLocal is empty");
+
         RuleFor(a => a.ElapsedTimeInSec).NotNull().WithMessage("ElapsedTimeInSec is not included in the body");
         RuleFor(a => a.ElapsedTimeInSec).NotEmpty().WithMessage("ElapsedTimeInSec is empty");
         RuleFor(a => a.ElapsedTimeInSec).GreaterThanOrEqualTo(0).WithMessage("ElapsedTimeInSec can't be negative");
+
         RuleFor(a => a.DistanceInMeters).NotNull().WithMessage("DistanceInMeters is not included in the body");
         RuleFor(a => a.DistanceInMeters).NotEmpty().WithMessage("DistanceInMeters is empty");
         RuleFor(a => a.DistanceInMeters).GreaterThanOrEqualTo(0).WithMessage("DistanceInMeters can't be negative");
+        
         RuleFor(a => a.AthleteId).NotNull().WithMessage("AthleteId is not included in the body");
         RuleFor(a => a.AthleteId).NotEmpty().WithMessage("AthleteId is empty");
 
--- RunAholicAPI/Validation/AthleteValidator.cs
+++ RunAholicAPI/Validation/AthleteValidator.cs
@@ -4,20 +4,20 @@ public class AthleteValidator : AbstractValidator<Athlete>
 {
     public AthleteValidator()
     {
-        RuleFor(a => a.FirstName).NotEmpty().WithMessage("FirstName is empty");
         RuleFor(a => a.FirstName).NotNull().WithMessage("FirstName is not included in the body");
+        RuleFor(a => a.FirstName).NotEmpty().WithMessage("FirstName is empty");
         
-        RuleFor(a => a.LastName).NotEmpty().WithMessage("LastName is empty");
-        RuleFor(a => a.LastName).NotNull().WithMessage("LastName is not included in the body");
+        RuleFor(a => a.LastName).NotNull().WithMessage("LastName is empty");
+        RuleFor(a => a.LastName).NotEmpty().WithMessage("LastName is not included in the body");
         
-        RuleFor(a => a.City).NotEmpty().WithMessage("City is empty");
-        RuleFor(a => a.City).NotNull().WithMessage("City is not included in the body");
+        RuleFor(a => a.City).NotNull().WithMessage("City is empty");
+        RuleFor(a => a.City).NotEmpty().WithMessage("City is not included in the body");
         
-        RuleFor(a => a.Country).NotEmpty().WithMessage("Country is empty");
-        RuleFor(a => a.Country).NotNull().WithMessage("Country is not included in the body");
+        RuleFor(a => a.Country).NotNull().WithMessage("Country is empty");
+        RuleFor(a => a.Country).NotEmpty().WithMessage("Country is not included in the body");
         
-        RuleFor(a => a.Age).NotEmpty().WithMessage("Age is empty");
-        RuleFor(a => a.Age).NotNull().WithMessage("Age is not included in the body");
+        RuleFor(a => a.Age).NotNull().WithMessage("Age is empty");
+        RuleFor(a => a.Age).NotEmpty().WithMessage("Age is not included in the body");
         RuleFor(a => a.Age).GreaterThan(6).WithMessage("Age has to be older than 6");
     }
 }
